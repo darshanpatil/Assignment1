64b87dc2c58e358fb8ee549284a3c0ec3adc9ed5
Arvind Narain <ops@mobileasap.com>
2015-06-09 15:41:12 +0530

    Changes - added XML file generation.

--- First Simple App/server.js
+++ First Simple App/server.js
@@ -1,9 +1,10 @@
 var jsonObj = require("./Source/source.json");
 var express = require("express");
+var js2xmlparser = require("js2xmlparser");
 var fs = require('fs');
 
 var app = express();
-var obj = jsonObj.students;
+var obj = jsonObj.student;
 
 app.get('/',function(req,res){
 	//Store array data in file
@@ -11,12 +12,8 @@ app.get('/',function(req,res){
 	for(var stud in obj) {
 		data  = data + obj[stud].id + "	|	" + obj[stud].fName + "		|	" + obj[stud].lName + "			|	" + obj[stud].score + "\n";
 	}
-	fs.writeFile("./Source/destination.txt", data, function(err) {
-		if(err) throw err;
-		console.log("[INFO] Simple file write complete.");
-	});
+	writeToFile("./Source/destination.txt", data);
 	
-	//console.log(obj);
 	//Sort JSON by field
 	obj.sort(sort_by('fName', false, function(a){return a.toUpperCase()}));
 
@@ -25,14 +22,22 @@ app.get('/',function(req,res){
 	for(var stud in obj) {
 		sortedData  = sortedData + obj[stud].id + "	|	" + obj[stud].fName + "		|	" + obj[stud].lName + "			|	" + obj[stud].score + "\n";
 	}
-	fs.writeFile("./Source/sortedDataDestination.txt", sortedData, function(err) {
-		if(err) throw err;
-		console.log("[INFO] Sorted data file write complete.");
-	});
+	writeToFile("./Source/sortedDataDestination.txt", sortedData);
 	
-	//console.log(obj);
+	//Generate XML from JSON and store it in file
+	var xmlData = js2xmlparser("Students", jsonObj);
+	writeToFile("./Source/XMLDataDestination.xml", xmlData);
+	
 	res.end("Write Complete !!!");
 });
+
+//File write method
+function writeToFile(fileName, data) {
+	fs.writeFile(fileName, data, function(err) {
+		if(err) throw err;
+		console.log("[INFO] File write complete.");
+	});
+}
 
 //Sort array by any columnName, sortOrder, dataType(for int pass 'parseInt', for float pass 'parseFloat')
 var sort_by = function(field, reverse, primer){
